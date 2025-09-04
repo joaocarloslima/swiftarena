a1e64d5ed3f5d87e901c0a6b16558a341200169d
joaocarloslima <joaocarloslima@me.com>
2025-09-04 19:42:31 -0300

    add user avatar and search functionality to student card view

--- src/main/java/br/com/fiap/swiftarena/adm/AdmController.java
+++ src/main/java/br/com/fiap/swiftarena/adm/AdmController.java
@@ -1,7 +1,9 @@
 package br.com.fiap.swiftarena.adm;
 
 import br.com.fiap.swiftarena.user.Role;
+import br.com.fiap.swiftarena.user.User;
 import br.com.fiap.swiftarena.user.UserRepository;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,8 +20,9 @@ public class AdmController {
     }
 
     @GetMapping("/adm")
-    public String adm(Model model) {
+    public String adm(Model model, @AuthenticationPrincipal User user) {
         model.addAttribute("students", admService.getStudentCards());
+        model.addAttribute("user", user);
         return "adm";
     }
 }
